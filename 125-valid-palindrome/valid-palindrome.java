fd6229644313f972bc6b220a5f95fcf1097a46b8
Hari krishna <harikrishna.m23sep@gmail.com>
2026-03-07 23:02:53 +0530

    Time: 2 ms (99.13%) | Memory: 44.1 MB (92.02%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,41 +1,41 @@
 class Solution {
     public boolean isPalindrome(String s) {
-    //     int last = s.length()-1;
-    //    for(int i=0;i<last;i++){
-    //     if(!Character.isLetterOrDigit(s.charAt(i))){
-    //         continue;
-    //     }
-    //     while (i < last && !Character.isLetterOrDigit(s.charAt(last))) {
-    //             last--;
-    //         }
+        int last = s.length()-1;
+       for(int i=0;i<last;i++){
+        if(!Character.isLetterOrDigit(s.charAt(i))){
+            continue;
+        }
+        while (i < last && !Character.isLetterOrDigit(s.charAt(last))) {
+                last--;
+            }
         
-    //     if (Character.toLowerCase(s.charAt(i)) !=
-    //             Character.toLowerCase(s.charAt(last))) {
-    //             return false;
-    //         }
+        if (Character.toLowerCase(s.charAt(i)) !=
+                Character.toLowerCase(s.charAt(last))) {
+                return false;
+            }
         
-    //         last--;
-    //     }
+            last--;
+        }
         
        
-    //    return true;
-        int left = 0;
-        int right = s.length() - 1;
+       return true;
+        // int left = 0;
+        // int right = s.length() - 1;
 
-        while (left < right) {
-            if (!Character.isLetterOrDigit(s.charAt(left))) {
-                left++;
-            } else if (!Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
-            } else {
-                if (Character.toLowerCase(s.charAt(left)) !=
-                    Character.toLowerCase(s.charAt(right))) {
-                    return false;
-                }
-                left++;
-                right--;
-            }
-        }
-        return true;
+        // while (left < right) {
+        //     if (!Character.isLetterOrDigit(s.charAt(left))) {
+        //         left++;
+        //     } else if (!Character.isLetterOrDigit(s.charAt(right))) {
+        //         right--;
+        //     } else {
+        //         if (Character.toLowerCase(s.charAt(left)) !=
+        //             Character.toLowerCase(s.charAt(right))) {
+        //             return false;
+        //         }
+        //         left++;
+        //         right--;
+        //     }
+        // }
+        // return true;
     }
 }
